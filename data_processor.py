ac2739a27c7942a2e269e517d603c111fe509e44
Sumeet Saini <sumeetsaini621@gmail.com>
2023-01-25 22:59:46 +0000

    cleans and adds readme

--- data_processor.py
+++
@@ -1,8 +0,0 @@
-import API_pull
-import pandas as pd
-
-data = API_pull.getBookingData()['Table1']
-df = pd.DataFrame.from_dict(data, orient='columns')
-print(df[['startdatetime', 'enddatetime']])
-
-df.loc[df['startdatetime'] == '2022-01-06T04:16:00']
